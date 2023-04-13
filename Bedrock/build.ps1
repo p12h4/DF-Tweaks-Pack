d625e4d10bf09a1bdb2d739c7d3f1a94ae03972c
p3sw <110542328+p12h4@users.noreply.github.com>
2023-04-13 15:21:22 +0200

    copy folders instead of overwriting

--- Bedrock/build.ps1
+++ Bedrock/build.ps1
@@ -4,7 +4,7 @@ $cd = $PSScriptRoot
 function CompressJSONs($Paths) {
    $results = ""
    ForEach ($Path in $Paths) {
-      if (Test-Path "$Path") {
+      if ([bool](Test-Path -Path "$Path") -eq $True) {
          $results = (Get-ChildItem -ErrorAction Continue -Recurse -Path "$Path" -Filter *.json).FullName
          Foreach ($result in $results) {
             (Get-Content -ErrorAction Continue -Force -Path "$result") -join ' ' | % {$_.replace(" ","")} | % {$_.replace("`t","")} | % {$_.replace("`n","")} | Set-Content -ErrorAction Continue -Force -Path "$result"
@@ -14,11 +14,17 @@ function CompressJSONs($Paths) {
 }
 
 function BuildPack($Folders) {
+   if ([bool](Test-Path -Path "$cd\temp") -eq $False) {
+      mkdir -Force "$cd\temp" | Out-Null
+   }
    ForEach ($Folder in $Folders) {
-      CompressJSONs "$Folder\attachables", "$Folder\entity", "$Folder\items", "$Folder\models\entity", "$Folder\render_controllers"
-      [System.IO.Compression.ZipFile]::CreateFromDirectory("$cd\$Folder", "$cd\Discontinued.Feature.Tweaks.$Folder.zip")
+      Write-Host "Adding Folder: `"$Folder`" to `"$cd\Discontinued.Feature.Tweaks.$Folder.mcpack`""
+      Copy-Item -Recurse -Force -Path "$Folder" -Destination "$cd\temp"
+      CompressJSONs "$cd\temp\$Folder\attachables", "$cd\temp\$Folder\entity", "$cd\temp\$Folder\items", "$cd\temp\$Folder\models\entity", "$cd\temp\$Folder\render_controllers"
+      [System.IO.Compression.ZipFile]::CreateFromDirectory("$cd\temp\$Folder", "$cd\Discontinued.Feature.Tweaks.$Folder.zip")
       Rename-Item -ErrorAction Continue -Force "$cd\Discontinued.Feature.Tweaks.$Folder.zip" "$cd\Discontinued.Feature.Tweaks.$Folder.mcpack"
    }
+   Remove-Item -Recurse -Force -Path "$cd\temp"
 }
 
 BuildPack "1.16.40", "1.18.10"
